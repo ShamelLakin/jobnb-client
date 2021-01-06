88c1a345b08a67003321a19082fbd7e34cd82e25
Shamel Lakin <christlife4u@gmail.com>
2021-01-05 20:43:03 -0500

    logo

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <div className='header'>
             <img
             className="header_icon"
-            src=""
+            src="https://lh3.googleusercontent.com/qGC17mUnzVgPQ7LXCbfIuiM1pJzWZ2Ricm3NgfrWBtrXOaqhB8SMDaujzubmEQROJXeZ5wPaVNewEwpyFhyFWf-zjHj078U8Qv_oOZR6aVOg26ixjJxwHX0OMeU-yjxHre6EeWxeY-M=w2400"
             alt=""
             />
 
